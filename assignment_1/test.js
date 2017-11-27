9ea4a5898f08fb5fcb728e98b8907d9cef0424b6
Kimberly Gonzales <33404920+KimGonzales@users.noreply.github.com>
2017-11-27 17:03:36 -0500

    Assignment 1

---
+++ assignment_1/test.js
@@ -0,0 +1,18 @@
+console.log('test')
+/* Hands On Excercises
+Exercise 1 */
+var name = "Kim";
+console.log("Hello " + name);
+//Excersise 2
+x = 1125; var answer = (x%2);
+console.log(answer);
+var a = true; var b = false; var c = true;
+console.log (a || b || c);
+console.log(a || c);
+console.log (a || b);
+console.log(a && b && c);
+console.log(a&&b);
+String(12);
+parseInt("113");
+var hash = "#";
+
--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -1,17 +1,28 @@
 // Exercise #1
 // Write your solution below
+var hashtag = '';
 
+for (var counter = 0; counter <7; counter = counter + 1) {
+    hashtag = hashtag + '#';
+    console.log(hashtag);
+}
 
 /* 
 Write your explanation for Exercise #1 here
-
+The hashtag variable is initially given an empty string value ('') so 
+that once the loop begins, the printed result will start at ('' + #) = #. Afterwards, 
+the 'hashtag's to print will have one additonal "#" and so forth. 
+The counter variable is give an inital value of 0. Every time the program loops,
+the counter will be supplemented with a value of 1, up until it reaches 7. 
 */
 
 
 
 
 
-// Exercise #2
+/* Exercise #2 (write a function that checks 
+    whether a number is odd or even)
+*/
 // Write your solution below
 
 
